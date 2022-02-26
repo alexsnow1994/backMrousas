c8e073bbb6ba8229f7d8a8b89e271bb8754a30f3
Alejandro Estrada <ing.estrada.rocha@gmail.com>
2022-02-25 20:44:21 -0600

    api completa

--- routes/books.js
+++ routes/books.js
@@ -16,49 +16,7 @@ router.post("/createbook",bookcontroller.createbook);
 router.delete("/:id",bookcontroller.delatebook);
 
 router.put("/:id", bookcontroller.update);
-// router.get('/', async(req, res) => {
-//     let book = await Book.find();
-//     res.json(book);
-// })
-// router.post('/', async(req, res) => {
-//     const { title, author, isbn } = req.body;
-//     //const imagePath = '/uploads/'+req.file.filename;
-//     const newbook = new Book({ title, author, isbn});
-//     await newbook.save()
-//     console.log(newbook);
-//     res.json({ message: 'save book', data:newbook });
-// })
-// router.put('/:id', async(req,res)=>{
-//     console.log(req.params.id);
-//     console.log(req.body);
-//     const { title, author, isbn } = req.body;
-//     const imagePath = '/uploads/'+req.file.filename;
-//     let book= await Book.findById(req.params.id);
-//     book.title = title
-//     book.author = author
-//     book.isbn = isbn
-//     book.imagePath = imagePath
-//     book.save()
-//     try {
-//         outputFile(path.resolve('./backEnd/public'+book.imagePath));
-//     } catch (error) {
-//         console.log(error);
-//     }
-//       console.log(book);
-//       res.json({messaje:'Done'});
-      
-    
-//   });
 
-// router.delete('/:id', async(req, res) => {
-//     const book = await Book.findByIdAndDelete(req.params.id);
-//    try {unlink(path.resolve('./backEnd/public'+book.imagePath));}
-//    catch(err){
-//        console.log(err);
-//    }
-//     console.log(book);
-//     res.json({ message: 'delating' })
-// })
 
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,33 +1,4 @@
-// const { Router } = require('express');
-// const router = Router();
 
-
-// const user = require('../models/Book')
-
-// router.get('/', async(req, res) => {
-//     let book = await user.find();
-//     res.json(user);
-// })
-// router.post('/', async(req, res) => {
-//     const { usuario, email, password } = req.body;
-    
-//     const newuser = new user({ title, author, isbn, imagePath });
-//     await newuser.save()
-//     console.log(newbook);
-//     res.json({ message: 'save book' });
-// })
-// router.update('/:id', async(req, res)=> {
-
-// });
-
-// router.delete('/:id', async(req, res) => {
-//     const book = await user.findByIdAndDelete(req.params.id)
-//     console.log(book);
-//     res.json({ message: 'delating' })
-// })
-
-
-// module.exports = router;
 const express	= require("express")
 const router	= express.Router()	
 
